6a74094cad2dc2f73b0c083abe9268b0f4b97511
Kaushik IB <kaushik.itagi@kohls.com>
2018-05-04 11:45:32 +0530

    Compatiblity issue resolved

---
+++ app/components/github.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {GithubService} from '../services/github.service';
+
+@Component({
+    moduleId:module.id,
+    selector: 'github',
+    templateUrl: 'github.component.html',
+    providers:[GithubService]
+})
+export class GithubComponent { 
+    user:any;
+    repos:any;
+    username:string;
+    
+    constructor(private _githubService:GithubService){
+        console.log('Github Component Init...');   
+    }
+    
+    search(){
+        this._githubService.updateUsername(this.username);
+        
+        this._githubService.getUser().subscribe(user => {
+            //console.log(user);
+            this.user = user;
+        });
+        
+        this._githubService.getRepos().subscribe(repos => {
+            this.repos = repos;
+        });
+    }
+}
---
+++ app/services/github.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+    private username = 'bradtraversy';
+    private client_id = 'd9308aacf8b204d361fd';
+    private client_secret='62551cc02cee983fff0bac41baf170eb5a312c1c';
+    
+    constructor(private _http:Http){
+        console.log('Github Service Init...');
+    }
+    
+    getUser(){
+        return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+    
+    getRepos(){
+        return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+    
+    updateUsername(username:string){
+        this.username = username;
+    }
+}
